faff228f47c00237522beec230787b7a6dc6d3dc
Jinrui Wang <jinrui.w@outlook.com>
2025-09-26 15:00:50 +0100

    bio update

--- src/lib/components/Bio.svelte
+++ src/lib/components/Bio.svelte
@@ -8,13 +8,15 @@
       
     <p>Hi, I’m Jinrui Wang (王金睿). </p>
     <p>
-        I’m entering my thrid year of PhD studies supervised by <a class="med-link" href="http://www.utahinrichs.de/" target="_blank">Dr. Uta Hinrichs</a> and <a class="med-link" href="https://vishub.net/bach" target="_blank">Dr. Benjamin Bach</a> at <a class="med-link" href="https://vishub.net" target="_blank">Visual+Interactive Lab</a>, The University of Edinburgh. My research focuses on visualization atlases as an emerging form of open data visualizations and effective design of public data representation. I'm passionate about practical project in data exploration and interactive visualization design.
+        I’m entering my thrid year of PhD studies supervised by <a class="med-link" href="http://www.utahinrichs.de/" target="_blank">Dr. Uta Hinrichs</a> and <a class="med-link" href="https://vishub.net/bach" target="_blank">Dr. Benjamin Bach</a> at <a class="med-link" href="https://vishub.net" target="_blank">Visual+Interactive Lab</a>, The University of Edinburgh. Before that, I completed my MSc in Design Informatics (with distinction) at the University of Edinburgh (2022) and BEcon at University of International Business and Economics in Beijing (2020). My PhD research focuses on visualization atlases as an emerging form of visualization for communicating complex datasets to wide range of audiences. I'm passionate about research through practical designs in interactive data exploration.
+        
+        <!-- My research focuses on visualization atlases as an emerging form of open data visualizations and effective design of public data representation. I'm passionate about practical project in data exploration and interactive visualization design. -->
     </p>
 
-    <p class="prior">Prior to this...</p>
+    <p class="prior">My ongoing projects👩🏻‍💻:</p>
 
-    <p class="bullet-point"> Visualization developer (RA) with <a href="https://peacerep.org" target="_blank" class="med-link">PeaceRep</a>, Edinburgh Law School</p>
-    <p class="bullet-point"> 🎓 MSc Design Informatics (with distinction), The University of Edinburgh</p>
+    <p class="bullet-point"> Designing a visualization atlas on the social co-benefits of climate mitigation datasets with <a href="https://edinburghcentre.org" target="_blank" class="med-link">Edinburgh Climate Change Insitute</a>.</p>
+    <p class="bullet-point"> Visualizing peace transistion data with <a href="https://peacerep.org" target="_blank" class="med-link">PeaceRep</a>, Edinburgh Law School.</p>
 
 </section>
 
